db5f74dce5db02ce0d55dad1f1b76af590227d20
Kieu Tuan Minh <kieutuanminh1708@gmail.com>
2026-05-13 22:39:34 +0700

    init project

---
+++ FlippedClassroom/src/main/java/com/group4/education/flippedclassroom/FlippedClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.group4.education.flippedclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlippedClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlippedClassroomApplication.class, args);
+	}
+
+}
---
+++ FlippedClassroom/src/test/java/com/group4/education/flippedclassroom/FlippedClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group4.education.flippedclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlippedClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
