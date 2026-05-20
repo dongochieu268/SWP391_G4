f3f35aeef0d150e0e77284b493d7bb21d4099a92
admin <dlnf2k6mt@gmail.com>
2026-05-21 01:25:22 +0700

    complete node content management

---
+++ src/main/java/com/edunac/mentora/controller/NodeContentController.java
@@ -0,0 +1,34 @@
+package com.edunac.mentora.controller;
+
+import com.edunac.mentora.domain.NodeContent;
+import com.edunac.mentora.service.LearningNodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/teacher/learning-path/node-content")
+public class NodeContentController {
+
+    @Autowired
+    private LearningNodeService nodeService;
+
+    @PostMapping("/save")
+    public String saveContent(@ModelAttribute NodeContent content,
+                              @RequestParam Integer nodeId,
+                              @RequestParam Integer pathId) {
+
+        nodeService.saveContentToNode(nodeId, content);
+
+        return "redirect:/teacher/learning-path/" + pathId + "/nodes";
+    }
+
+    @GetMapping("/delete/{contentId}")
+    public String deleteContent(@PathVariable Integer contentId,
+                                @RequestParam Integer pathId) {
+
+        nodeService.deleteContent(contentId);
+
+        return "redirect:/teacher/learning-path/" + pathId + "/nodes";
+    }
+}
